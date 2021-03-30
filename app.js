105503af024b90bdd5452f136bd60cba424c804e
mahidulmoon <mahidulmoon@gmail.com>
2021-03-30 22:47:52 +0600

    module implementation

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
+var log = require('./logger');
+
 function sayHello(name){
     console.log('Hello'+name);
 }
 
 sayHello('Moon');
+
+console.log(log);
+
+logger.log('message'); //module description
