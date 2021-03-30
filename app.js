f00f45489861477cd7a9b333e4feb2933ced4307
mahidulmoon <mahidulmoon@gmail.com>
2021-03-30 23:04:29 +0600

    before node, js was able to run in browser not in console

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 var logger = require('./logger');
+const os = require('os');
+
+var totalmemory = os.totalmem();
+var freememory = os.freemem();
 
 function sayHello(name){
     console.log('Hello'+name);
 }
 
 sayHello('Moon');
+console.log(`Total Memory : ${totalmemory},and Free Memory: ${freememory}`)
 
 console.log(logger);
 
