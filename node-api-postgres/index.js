53765aa098d5cc6aca1c9457631f239f017fa69a
mahidulmoon <mahidulmoon@gmail.com>
2021-03-31 21:44:35 +0600

    crud api done

--- node-api-postgres/index.js
+++ node-api-postgres/index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
+//queris import
+const db = require('./queries');
 
 
 //adding middleware
@@ -17,6 +19,14 @@ app.use(
 app.get('/', (request, response) => {
     response.json({ info: 'Node.js, Express, and Postgres API' })
   })
+
+
+//crud api
+app.get('/users', db.getUsers)
+app.get('/users/:id', db.getUserById)
+app.post('/users', db.createUser)
+app.put('/users/:id', db.updateUser)
+app.delete('/users/:id', db.deleteUser)
 
 
 // server setup
